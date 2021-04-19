e1cc159dafc47de634b4452309359e7ddd47f3be
dynamusdev <alexandrenascimento@live.com>
2021-04-19 18:17:34 -0300

    :sparkles: input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,44 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles';
-import { translate } from '../../i18n';
+import { Container, IconContainer, InputView, NullContainer, MaskeredInputView } from './styles';
+import { Icon } from '../icon'
 
 interface Props {
-  label?: string,
-  labelTx?: string,
   value?: string,
-  onChangeText?: any,
-  mode?: 'flat' | 'outlined',
-  left?: React.ReactNode,
-  right?: React.ReactNode,
-  disabled?: boolean,
+  onChangeText?: (value) => void,
+  mode?: 'userInput' | 'passwordInput' | 'phoneInput' | 'cpfInput',
   placeholder?: string,
   placeholderTx?: string,
-  selectionColor?: string,
-  underlineColor?: string,
-  error?: boolean,
   multiline?: boolean,
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters',
   numberOfLines?: number,
-  onFocus?: any
+  keyboardType?: | 'ascii-capable' | 'numbers-and-punctuation' | 'url' | 'name-phone-pad' | 'twitter'
+  | 'web-search' | 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'number-pad' | 'decimal-pad';
+
 }
 
 export function Input(props: Props) {
+  const [borderColor, setBorderColor] = useState('#AEAEAE')
+  const [showPass, setShowPass] = useState(false)
   return (
-    <Container
-      label={translate(props.labelTx) || props.label}
-      value={props.value}
-      onChangeText={props.onChangeText}
-      mode={props.mode || 'flat'}
-      left={props.left}
-      right={props.right}
-      disabled={props.disabled}
-      placeholder={translate(props.placeholderTx) || props.placeholder}
-      selectionColor={props.selectionColor}
-      underlineColor={props.underlineColor}
-      error={props.error}
-      multiline={props.multiline}
-      numberOfLines={props.numberOfLines}
-      onFocus={props.onFocus}
-    />
+    <Container style={{ borderColor: borderColor }} >
+      {
+        props.mode ?
+          <IconContainer>
+            <Icon
+              name={props.mode === 'userInput' || props.mode === 'cpfInput' ?
+                'user' : props.mode === 'passwordInput' ?
+                  'lock' : props.mode === 'phoneInput' && 'phone'
+              }
+              color='#fff'
+            />
+          </IconContainer>
+          :
+          <NullContainer />
+      }
+
+      {
+        props.mode === 'cpfInput' ?
+          <MaskeredInputView
+            maskered={'cpf'}
+            placeholder={props.placeholder}
+            onChangeText={props.onChangeText}
+            onFocus={() => setBorderColor('#008AD0')}
+            value={props.value}
+          />
+          :
+          <InputView
+            placeholder={props.placeholder}
+            onChangeText={props.onChangeText}
+            autoCapitalize={props.autoCapitalize || 'none'}
+            value={props.value}
+            onFocus={() => setBorderColor('#008AD0')}
+            keyboardType={props.keyboardType || 'default'}
+            secureTextEntry={props.mode === 'passwordInput' && !showPass}
+          />
+      }
+
+      <NullContainer style={{width: props.mode !== 'passwordInput' && 10 }}>
+        {
+          props.mode === 'passwordInput' &&
+          <Icon
+            name={showPass ? 'eye-off' : 'eye'}
+            color='#008AD0'
+            onPress={() => setShowPass(!showPass)}
+          />
+        }
+      </NullContainer>
+    </Container>
   )
 }
--- src/components/input/styles.tsx
+++ src/components/input/styles.tsx
@@ -1,6 +1,59 @@
 import styled from 'styled-components/native';
-import { TextInput } from 'react-native-paper';
+import { TextInputMask } from "react-native-masked-text";
 
-export const Container = styled(TextInput).attrs({})`
-  
+export const Container = styled.View`
+  width: 300px;
+  height: 45px;
+  background: #FFFFFF;
+  border: 1px solid #AEAEAE;
+  border-radius: 5px;
+  margin-bottom: 14px;
+
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const IconContainer = styled.View`
+  height: 100%;
+  width: 48px;
+
+  background: #008AD0;
+
+  align-items: center;
+  justify-content: center;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const InputView = styled.TextInput`
+  width: 200px;
+  height: 100%;
+  padding: 5px;
+  font-size: 14px;
+  text-align: center;
+`;
+
+export const MaskeredInputView = styled(TextInputMask).attrs(props => ({
+  type: props.maskered || "custom",
+  options: {
+    mask:
+      props.typeMask ||
+      "******************************************************************************************************"
+  },
+  editable: props.edit || true,
+}))`
+  flex: 1;
+  font-size: 14px;
+  text-align: center;
+`;
+
+export const NullContainer = styled.View`
+  height: 100%;
+  width: 48px;
+
+  background: #fff;
+
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
 `;
