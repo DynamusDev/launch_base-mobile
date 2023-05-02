ec2425e661c6b4ac77213ccdba28e32fb458922c
Alexandre Nascimento <alexandrenascimento@MacBook-Pro-Amaris.local>
2023-05-01 22:04:18 -0300

    Update de pacote

--- App.tsx
+++ App.tsx
@@ -1,29 +1,31 @@
-if(__DEV__) {
-  import('./src/config/ReactotronConfig').then(() => console.log('Reactotron Configured'))
-}
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from 'styled-components/native';
-import { ContextProvider } from './src/hooks/useTranslate';
+// if (__DEV__) {
+//   import("./src/config/ReactotronConfig").then(() =>
+//     console.log("Reactotron Configured")
+//   );
+// }
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "styled-components/native";
+import { ContextProvider } from "./src/hooks/useTranslate";
 
-import { dark, ligth } from './src/theme'
-import { AuthStack, DashboardTabs } from './src/routes'
+import { dark, ligth } from "./src/theme";
+import { AuthStack, DashboardTabs } from "./src/routes";
 
 export default function App() {
-  const [language, setLanguage] = useState({})
-  
+  const [language, setLanguage] = useState({});
+
   useEffect(() => {
-    setLanguage('en')
-  }, [])
+    setLanguage("en");
+  }, []);
 
   const contextProps: object = {
     language,
-    setLanguage
-  }
+    setLanguage,
+  };
   return (
     <ContextProvider {...contextProps}>
-      <ThemeProvider theme={dark} >
+      <ThemeProvider theme={dark}>
         <NavigationContainer>
           <DashboardTabs />
         </NavigationContainer>
